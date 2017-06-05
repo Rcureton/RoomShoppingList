93615d7ef4d3107b94e5f99d992ee5c953080c85
Rashad cureton <rashad.cureton@gmail.com>
2017-06-05 10:10:10 -0400

    Basic project setup and added Room and DAOs libraries

---
+++ app/src/main/java/com/bignerdranch/android/roomshoppinglist/ShoppingListActivity.java
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.roomshoppinglist;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class ShoppingListActivity extends SLSingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return ShoppingListFragment.newInstance();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_shopping_list);
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/roomshoppinglist/ShoppingListFragment.java
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.roomshoppinglist;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ShoppingListFragment extends Fragment {
+
+    public static ShoppingListFragment newInstance() {
+        return new ShoppingListFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
